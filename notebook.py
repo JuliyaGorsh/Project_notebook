99f3e13cbd9015f6c2a5c9c148a69a4c0884b649
Yuliya <kuv_96@mail.ru>
2023-03-16 05:07:57 +0300

    find_note

--- notebook.py
+++ notebook.py
@@ -22,3 +22,18 @@ def remove_note():
     confirm = input(f'Вы точно хотите удалить заметку "{notebook[id-1][1]}"? (y/n)')
     if confirm.lower() == 'y':
         del_note = notebook.pop(id-1)
+
+def find_note():
+    global notebook
+    string = input('Для поиска введите необходимые значения: ')
+    x = 0
+    for note in notebook:
+        for i in range(len(note)):
+            for j in range(len(note[i])):
+                if string.lower() in str(note[i][j : j + len(string)]).lower():
+                    x = 1
+                    print(*note)
+                    break
+    if x != 1:
+        print('Ничего не найдено')
+    print()
