95812b66295b95c84036d396406764800d68f4d4
Yuliya <kuv_96@mail.ru>
2023-03-16 05:02:32 +0300

    str_to_list

--- data_base.py
+++ data_base.py
@@ -6,3 +6,10 @@ def list_to_str(notebook: list) -> str:
         new_note = ';'.join(note)
         str_notebook += new_note + '\n'
     return str_notebook[:-1]
+
+def str_to_list(notebook: list) -> list:
+    new_list = []
+    for note in notebook:
+       new_note = note.strip().split(';')
+       new_list.append(new_note)
+    return new_list
