dceedf76f40909b71e4ad4c77f3701f85439d28a
Yuliya <kuv_96@mail.ru>
2023-03-16 05:06:43 +0300

    remove_note

--- notebook.py
+++ notebook.py
@@ -1,4 +1,5 @@
-
+import view
+from datetime import datetime as dt
 
 notebook = []
 
@@ -14,3 +15,10 @@ def add_note():
     global notebook
     note = view.input_new_note()
     notebook.append(note)
+
+def remove_note():
+    global notebook
+    id = view.input_remove_note()
+    confirm = input(f'Вы точно хотите удалить заметку "{notebook[id-1][1]}"? (y/n)')
+    if confirm.lower() == 'y':
+        del_note = notebook.pop(id-1)
